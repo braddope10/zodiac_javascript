aba65e44017152ff53bc0f92df4fcf803751236d
github username <github email address>
2020-07-31 19:17:14 -0700

    Created constructor function for User

--- frontend/src/user.js
+++ frontend/src/user.js
@@ -1,3 +1,9 @@
 class User{
-    
+    constructor(name, username, email, bday, sign_id){
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.bday = bday;
+        this.sign_id = sign_id;
+    }
 }
